d4796b5c254cf2b858678c308eb4a3deaad2e1a7
Abdullah Hameed <61628144+abdullahkhan010@users.noreply.github.com>
2023-03-24 20:45:13 +0500

    Add files via upload

--- i191881_i191883_A02.py
+++ i191881_i191883_A02.py
@@ -9,25 +9,10 @@ from sklearn.preprocessing import PolynomialFeatures
 from psx import stocks, tickers
 import datetime
 
-from psx import stocks, tickers
-import pandas as pd
-import datetime
-
-tickers = tickers()
-
-# data = stocks("ZTL", start=datetime.date(2018, 1, 1), end=datetime.date.today())
-
-data = stocks(["ZELP", "ZAHID"], start=datetime.date(2018, 1, 1), end=datetime.date.today())
-
-print(data)
-
-data.to_csv(r'result.csv')
-data.head(10)
-
 app = Flask(__name__, template_folder='/app')
 
 # Load the dataset
-# tickers = tickers()
+tickers = tickers()
 file_path = "result.csv"
 
 if os.path.exists(file_path):
@@ -35,7 +20,11 @@ if os.path.exists(file_path):
     print(f"{file_path} has been deleted.")
 else:
     print(f"{file_path} does not exist.")
-    
+
+
+
+
+
 data = stocks(["ZELP", "ZAHID"], start=datetime.date(2018, 1, 1), end=datetime.date.today())
 data.to_csv(r'result.csv')
 data = pd.read_csv('result.csv')
@@ -43,6 +32,33 @@ data = pd.read_csv('result.csv')
 train_size = int(len(data) * 0.8)
 train_data = data.iloc[:train_size]
 test_data = data.iloc[train_size:]
+
+# Define the input features (Open, High, Low, and Volume)
+X_train = train_data[['Open', 'High', 'Low', 'Volume']]
+X_test = test_data[['Open', 'High', 'Low', 'Volume']]
+
+# Define the target variable (Close)
+y_train = train_data['Close']
+y_test = test_data['Close']
+
+# Create polynomial features of degree 2
+poly = PolynomialFeatures(degree=2)
+X_train_poly = poly.fit_transform(X_train)
+X_test_poly = poly.transform(X_test)
+
+# Train the polynomial regression model
+model = LinearRegression()
+model.fit(X_train_poly, y_train)
+
+# Print the model's mean squared error on the test data
+y_pred = model.predict(X_test_poly)
+mse = mean_squared_error(y_test, y_pred)
+
+# Save the trained model
+joblib.dump(model, 'model.pkl')
+
+# Load the trained model
+model = joblib.load('model.pkl')
 
 @app.route('/')
 def chart():
@@ -56,7 +72,7 @@ def chart():
     mae = mean_absolute_error(y_test, y_pred)
 
     # Render the template with the chart data and evaluation metrics
-    return render_template('template.html', chart_data=chart_data, r2=r2, mse=mse, mae=mae)
+    return render_template('index.html', chart_data=chart_data, r2=r2, mse=mse, mae=mae)
 
 @app.route('/input')
 def myinput():
@@ -82,6 +98,6 @@ def predict():
     except:
         # Render the prediction template with an error message if the input features are invalid
         return render_template('prediction.html', prediction=0, output="Enter input features in the previous page")
-    
-    if __name__ == "__main__":
+
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000,debug=True)
